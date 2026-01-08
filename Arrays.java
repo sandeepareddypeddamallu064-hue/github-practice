a6d5b8d269ca4f8b4fcfb0906c43e6289620f011
Sandeepa Reddy <sandeepareddypeddamallu064@gmail.com>
2026-01-08 10:43:45 +0530

    Rename Arrays.java.txt to Arrays,java

--- Arrays.java
+++ Arrays.java
@@ -4,16 +4,18 @@ import java.util.*;
 public class Main{
 public static void main(String[] args){
 Scanner sc = new Scanner(System.in);
-System.out.print(“How many days temperature’s? “);
+System.out.print(ï¿½How many days temperatureï¿½s? ï¿½);
 int n = sc.nextInt();
 int sum = 0;
 for(int i=1;i<=n;i++){
-System.out.print(“Day “+i+”’s temp? “);
+System.out.print(ï¿½Day ï¿½+i+ï¿½ï¿½s temp? ï¿½);
 int next = sc.nextInt();
 sum += next
 }
 double Average = (double) sum/n;
-System.out.println(“Average: “+average);
+System.out.println(ï¿½Average: ï¿½+average);
 }
 }
+
+
 
