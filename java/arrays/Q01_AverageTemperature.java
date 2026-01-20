4c8b94999d7659d418aff6053aa5152c344cd089
Sandeepa Reddy <sandeepareddypeddamallu064@gmail.com>
2026-01-20 19:22:23 +0530

    Added Q01 Average Temperature

---
+++ java/arrays/Q01_AverageTemperature.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Q01_AverageTemperature {
+public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+System.out.print("How many days temperature's? ");
+int n = sc.nextInt();
+int sum = 0;
+for(int i=1;i<=n;i++){
+System.out.print("Day "+i+ "'s temp? ");
+int next = sc.nextInt();
+sum += next;
+}
+double Average = (double) sum/n;
+System.out.println("Average: "+Average);
+}
+}
