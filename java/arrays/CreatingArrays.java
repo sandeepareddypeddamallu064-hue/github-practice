d4571c39bf12152bac3b3fba97c3bffbf90900f9
Sandeepa Reddy <sandeepareddypeddamallu064@gmail.com>
2026-01-22 05:46:25 +0530

    updated java arrays program

---
+++ java/arrays/CreatingArrays.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+public class CreatingArrays{
+    public static void main(String[] args){
+        int[] arr = new int[3];
+        arr[0] = 1;
+        arr[1] = 3;
+        arr[2] = 4;
+        System.out.println(Arrays.toString(arr));
+        //all together
+        String sArray[] = {"sammu","anvi"};
+        System.out.print(Arrays.toString(sArray));
+
+    }
+}
